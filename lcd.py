1e155e9e5188874c8977428fd14634371521f678
Gabriel Pellegrino <gabriel.pellegrino@student-cs.fr>
2020-12-08 23:43:40 +0100

    fix(remove log): too much log

--- lcd.py
+++ lcd.py
@@ -1,6 +1,3 @@
 class LCD1602:
-    def __init__(self):
-        print(" [.] Created LCD1602")
-
     def write_line(self, line, data, align='LEFT'):
         print(" [.] Write line", line, data, align)
