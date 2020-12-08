9593f0176fa62a5e0e4387f633ac8b47d7c63f52
Gabriel Pellegrino <gabriel.pellegrino@student-cs.fr>
2020-12-09 00:48:34 +0100

    fix(remove): too much log

--- ezblockvirtual/modules.py
+++ ezblockvirtual/modules.py
@@ -18,7 +18,7 @@ class GPS():
 
 class LCD1602:
     def __init__(self):
-        print(" [.] Created LCD1602")
+        pass
 
     def write_line(self, line, data, align='LEFT'):
         print(" [.] Write line", line, data, align)
