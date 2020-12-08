5a86e5c68cc7bdc7d814b78b52e1e9c4d2496e57
Gabriel Pellegrino <gabriel.pellegrino@student-cs.fr>
2020-12-08 21:47:19 +0100

    fix(name):

---
+++ __init__.py
@@ -0,0 +1,5 @@
+from . import modules
+
+__all__ = [
+    'modules'
+]
--- setup.py
+++ setup.py
@@ -1,22 +1,24 @@
-import setuptools
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+from setuptools import setup, find_packages
 
-setuptools.setup(
+with open('README.md') as readme_file:
+    README = readme_file.read()
 
-     name='ezblockVirtual',  
-     version='0.1',
-     scripts=['modules.py'] ,
-     author="gpspelle",
-     author_email="gpsunicamp016@gmail.com",
-     description="Sunfounder utility block to use virtual components",
-     long_description=long_description,
-     long_description_content_type="text/markdown",
-     url="https://github.com/gpspelle/ezblock-virtual-modules/blob/master/modules.py",
-     packages=setuptools.find_packages(),
-     classifiers=[
-         "Programming Language :: Python :: 3",
-         "License :: OSI Approved :: MIT License",
-         "Operating System :: OS Independent",
-     ],
- )
+with open('HISTORY.md') as history_file:
+    HISTORY = history_file.read()
+
+setup_args = dict(
+    name='ezblockvirtual',
+    version='0.1.3',
+    description="Sunfounder utility block to use virtual components",
+    long_description_content_type="text/markdown",
+    long_description=README + '\n\n' + HISTORY,
+    license='MIT',
+    packages=find_packages(),
+    author="gpspelle",
+    author_email="gpsunicamp016@gmail.com",
+    keywords=['Raspberrypi', 'Sunfounder', 'Ezblock'],
+    url="https://github.com/gpspelle/ezblock-virtual-modules/"
+)
+
+if __name__ == '__main__':
+    setup(**setup_args)
